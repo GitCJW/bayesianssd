6b8a05c14a8828b8b56526308f8715fa1981a4b8
cjw <c.waterkamp@gmx.de>
2026-03-23 18:39:59 +0100

    ADD workflow

---
+++ R/bayesianssd-package.R
@@ -0,0 +1,25 @@
+#' bayesianssd: Bayesian Sample Size Determination
+#'
+#' Provides tools for Bayesian sample size determination in clinical trials
+#' and other experimental settings. Includes functions for simulation-based
+#' design, posterior analysis, and decision criteria.
+#'
+#' @details
+#' This package implements methods for Bayesian adaptive design and
+#' sample size justification using prior and posterior predictive approaches.
+#'
+#' @author
+#' Christoph Waterkamp \email{christoph.waterkamp@uni-due.de}
+#' Jeannine Veseljak
+#'
+#' @seealso
+#' \code{\link{runSSD}},
+#' \link{https://github.com/GitCJW/bayesianssd}
+#'
+#' @docType package
+#' @name bayesianssd
+"_PACKAGE"
+
+## usethis namespace: start
+## usethis namespace: end
+NULL
